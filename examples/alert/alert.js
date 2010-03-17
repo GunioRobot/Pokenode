1407f1476bf16567298dc48619d0f1e56639c942
Viktor Kelemen <viktor@cirius.co.jp>
2010-03-17 18:08:39 +0900

    Refactoring the alert example

--- examples/alert/alert.js
+++ examples/alert/alert.js
@@ -1,1 +1,12 @@
 alert("Don't worry, file changed.");
+
+// we have to make a request to the server again
+var script = document.createElement("script"),
+    head = document.getElementsByTagName('head')[0],
+    rnd = Math.random() * 10000;
+
+if (head !== undefined) {
+    script.setAttribute("src","http://localhost:8080/?rnd=" + Math.round(rnd));
+    head.appendChild(script);
+}
+
