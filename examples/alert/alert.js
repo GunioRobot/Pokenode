69783e3486db34a5903a143db10f307dda9470de
Viktor Kelemen <viktor@cirius.co.jp>
2010-03-18 12:19:25 +0900

    Updating the alert example

--- examples/alert/alert.js
+++ examples/alert/alert.js
@@ -1,12 +1,22 @@
 alert("Don't worry, file changed.");
 
 // we have to make a request to the server again
-var script = document.createElement("script"),
-    head = document.getElementsByTagName('head')[0],
-    rnd = Math.random() * 10000;
+var script = document.getElementById("pokenode-server-callback"),
+    head,
+    rnd = Math.random() * 10000,
+    portNumber = 8080;
 
-if (head !== undefined) {
-    script.setAttribute("src","http://localhost:8080/?rnd=" + Math.round(rnd));
-    head.appendChild(script);
+// removing the old script tag
+if (script != null && script.parentNode != null) {
+    script.parentNode.removeChild(script);
 }
 
+// inserting the new one
+script = document.createElement("script");
+script.setAttribute("src","http://localhost:" + portNumber + "/?rnd=" + Math.round(rnd));
+head = document.getElementsByTagName('head')[0];
+
+if (head !== undefined) {
+    head.appendChild(script);
+}
+script.setAttribute("id","pokenode-server-callback");
