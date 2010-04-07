628c982c536390a4a61f20cdf456ae484bc134ab
Viktor Kelemen <viktor@cirius.co.jp>
2010-04-07 16:40:46 +0900

    Adding the remotePort attribute

--- examples/helloworld/config.js
+++ examples/helloworld/config.js
@@ -1,7 +1,9 @@
 exports.serverSettings = {
     port: 8080,
+    remotePort: 8888,
     files: [
         './examples/helloworld/index.html'
     ],
     callback: './examples/helloworld/reload.js'
+
 };
