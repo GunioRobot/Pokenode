642b321ccc42bdfae32ea47fe782bcc66cd818ef
Viktor Kelemen <viktor@cirius.co.jp>
2010-04-07 15:51:19 +0900

    Adding a proxy

--- pokeserver.js
+++ pokeserver.js
@@ -5,16 +5,23 @@ var http = require("http"),
     fs = require("fs"),
     responses = [],
     files = config.serverSettings.files || [],
-    callbackContent = "";
+    callbackContent = "",
+    url = require("url"),
+    parse = require('url').parse,
+    queryString = require('querystring'),
+    mime = require('./mime');
 
 
+var foreignHostPort = "4000";
 
-puts("   \\\\");
-puts("   (o>");
-puts("\\\\_//)");
-puts(" \\_/_)");
-puts("  _|_");
-puts("Pokeserver\n");
+var customContentFile = "pokeserver.html",
+    customContent = "";
+
+customContent = fs.readFileSync(customContentFile);
+
+
+// PokeNode logo
+drawLogo();
 
 puts("used port: " + config.serverSettings.port || 8080);
 puts("config file: " + configFile);
@@ -55,22 +62,56 @@ watchFiles(watchedFiles);
 // creating a http server
 http.createServer(function (req, res) {
 
-    responses.push(res);
+    switch (req.url) {
 
-    process.addListener("SIGINT", function () {
+        case "/pokenodeserver/callback":
 
-        // closing the responses
-        responses.forEach( function (response) {
+            responses.push(res);
 
-           response.close();
-        });
+            process.addListener("SIGINT", function () {
 
-        puts("good bye");
-        process.exit(0);
-    });
+                // closing the responses
+                responses.forEach( function (response) {
 
-    res.sendHeader( 200, {"Content-Type" : "text/javascript"} );
+                   response.close();
+                });
 
+                puts("good bye");
+                process.exit(0);
+            });
+
+            res.sendHeader( 200, {"Content-Type" : "text/javascript"} );
+
+            break;
+
+        case "pokenodeserver/admin":
+            // implementation later
+
+        default:
+
+            proxy(req, function (status, buffer, response) {
+
+                var endIndex = buffer.lastIndexOf("</body>"),
+                    head = buffer.substring(0, endIndex),
+                    tail = buffer.substring(endIndex, buffer.length),
+                    content = head + customContent + tail;
+
+                // modifying the length
+                var headers = response.headers;
+
+                headers["content-length"] = content.length;
+                delete headers["Content-Length"];
+
+                res.writeHead(response.statusCode, headers);
+
+                res.write(content);
+                res.close();
+            }, function (loc) {
+
+                res.sendHeader("302", { location: loc });
+                res.close();
+            });
+    }
 
 }).listen(config.serverSettings.port || 8080);
 
@@ -165,3 +206,143 @@ function watchFiles(files) {
         });
     });
 }
+
+function proxy(req, htmlCallback, nonHtmlCallback) {
+
+
+    var header = req.headers,
+        host = header.host;
+
+    var parsed = url.parse("http://"+host);
+
+    var path = req.url.split("."),
+        fileExtension = path[path.length - 1],
+        loc = "http://" + parsed.hostname + ":" + foreignHostPort + req.url,
+        sentData = "";
+
+    var mimeType = mime.types[fileExtension] || "text/html";
+
+
+    switch (mimeType) {
+
+      case "text/html":
+      case "application/xhtml+xml":
+
+            // if it is HTML, we insert our custom code
+
+            req.addListener("data", function (data) {
+                sentData += data;
+            });
+
+
+            req.addListener("end", function () {
+
+               // fetching the content from the server
+               request(req.method,"http://" + parsed.hostname + ":" + foreignHostPort + req.url, sentData, header, function (status, buffer, response) {
+                   puts("http://" + parsed.hostname + ":" + foreignHostPort + req.url);
+
+                   if (htmlCallback !== undefined) {
+
+                       htmlCallback(status, buffer, response);
+                   }
+               });
+            });
+
+            break;
+
+      default:
+
+        if (nonHtmlCallback !== undefined) {
+            nonHtmlCallback(loc);
+        }
+    }
+
+}
+
+
+/**
+ * Mega super awesome private request utility.
+ *
+ * @param  {string} method
+ * @param  {string} url
+ * @param  {hash} data
+ * @param  {hash} headers
+ * @param  {function} callback
+ * @param  {number} redirects
+ * @api private
+ */
+function request(method, url, data, headers, callback, redirects) {
+  var buf = '',
+      redirects = redirects || 3,
+      url = parse(url),
+      path = url.pathname || '/',
+      search = url.search || '',
+      hash = url.hash || '',
+      port = url.port || 80,
+      client = http.createClient(port, url.hostname);
+
+  headers.host = url.hostname;
+
+  if (headers.redirect) {
+      redirects = headers.redirect;
+      delete headers.redirect;
+  }
+
+   if (data) {
+
+        if (typeof data != 'string') {
+           data = queryString.stringify(data);
+        }
+
+        if (method === 'GET') {
+          search += (search ? '&' : '?') + data;
+        } else {
+          headers['content-length'] = data.length;
+          headers['content-type'] = 'application/x-www-form-urlencoded';
+        }
+  }
+
+  var req = client.request(method, path + search + hash, headers);
+
+  if (data && method !== 'GET') {
+      req.write(data);
+  }
+
+  req.addListener('response', function(res) {
+
+    if (req.statusCode < 200 || req.statusCode >= 400) {
+      callback(new Error('request failed with status ' + res.statusCode + ' "' + http.STATUS_CODES[res.statusCode] + '"'));
+
+    } else if (res.statusCode >= 300 && res.statusCode < 400) {
+
+      redirects--;
+      if (redirects > 0) {
+        request(method, res.headers.location, headers, data, callback, redirects);
+      } else {
+        callback(new Error('maximum number of redirects reached'));
+      }
+    } else {
+      res.setBodyEncoding('utf8');
+      res.addListener('data', function (chunk) {
+          buf += chunk ;
+      });
+
+      res.addListener('end', function () {
+          callback(null, buf, res);
+      });
+    }
+  });
+
+  req.close();
+}
+
+function drawLogo() {
+
+    puts("   \\\\");
+    puts("   (o>");
+    puts("\\\\_//)");
+    puts(" \\_/_)");
+    puts("  _|_");
+    puts("Pokeserver\n");
+}
+
